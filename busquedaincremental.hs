ca9cab3ee2439a02dc6b56dcca411e6b3cbf5ead
Josue FLores <josue5325@gmail.com>
2019-02-21 12:39:57 -0600

    programa busqueda incremental terminado

---
+++ busquedaincremental.hs
@@ -0,0 +1,14 @@
+z=[-10..100]
+
+buscar x = buscaArreglo(x,0,z)
+
+buscaArreglo (x,cont,z)=if(cont > (length z-1))then 
+    putStr "No encontrado"
+    else do{
+        if(z!!cont ==x)then do
+            putStr "encontrado en la posicion: \n"
+            print (cont)
+            putStr "\n"
+        else 
+                buscaArreglo(x,cont+1,z)
+    }
