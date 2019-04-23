3732480fe67676bd62cc6157bebcbf6a05fb2360
Josue Flores <josue5325@gmail.com>
2019-04-22 20:02:39 -0500

    recuperando mi archivo y cambiando archivo

--- opQuebrados.hs
+++ opQuebrados.hs
@@ -1,10 +1,17 @@
-suma x y = (div ((fst x)*(snd y) + (fst y)*(snd x)) (gcd ((fst x)*(snd y) + (fst y)*(snd x))((snd x)*(snd y))  ),div  ((snd x)*(snd y))(gcd ((fst x)*(snd y) + (fst y)*(snd x))((snd x)*(snd y))))
+parteAbajo x y = (lcm(snd x)(snd y))
 
-resta x y = (div ((fst x)*(snd y) - (fst y)*(snd x)) (gcd ((fst x)*(snd y) - (fst y)*(snd x))((snd x)*(snd y))  ),div  ((snd x)*(snd y))(gcd ((fst x)*(snd y) - (fst y)*(snd x))((snd x)*(snd y))))
+parteArriba x y =  (div (parteAbajo  x y)(snd x)) * (fst x) +  (div (parteAbajo  x y)(snd y)) * (fst y)
 
-multiplicacion x y = (div ((fst x)* (fst y)) (gcd ((fst x)* (fst y))((snd x)*(snd y))),div ((snd x)*(snd y)) (gcd ((fst x)* (fst y))((snd x)*(snd y))))
+sumares  x y =(div (parteArriba x y)(gcd (parteArriba x y)(parteAbajo x y)) ,div (parteAbajo x y) (gcd (parteArriba x y)(parteAbajo x y)))
 
-division x y = (div ((fst x)* (snd y)) (gcd ((fst x)* (snd y))((snd x)*(fst y))),div ((snd x)*(fst y)) (gcd ((fst x)* (snd y))((snd x)*(fst y))))
+parteArribares x y = (div (parteAbajo  x y)(snd x)) * (fst x) - (div (parteAbajo  x y)(snd y)) * (fst y)
 
+restares x y =(div(parteArribares x y)(gcd (parteArribares x y)(parteAbajo x y)), div (parteAbajo x y)(gcd(parteArribares x y )(parteAbajo x y)))
 
-           
+pareteArribaDiv x y = (fst x) * snd (y)
+pareteAbajoDiv x y = (snd x) * (fst y)
+resDivicion  x y = (div (pareteArribaDiv x y )(gcd (pareteArribaDiv x y )(pareteAbajoDiv x y)), div (pareteAbajoDiv x  y)(gcd(pareteArribaDiv x y)(pareteAbajoDiv x y)))
+
+paretArribaMult x y = (fst x)*(fst y)
+paretAbajoMult x y = (snd x)* (snd y)
+restMult x y =(div (paretArribaMult x y)(gcd (paretArribaMult x y)(paretArribaMult x y)) ,div(paretAbajoMult x y)(gcd(paretArribaMult x y)(paretAbajoMult x y)))
